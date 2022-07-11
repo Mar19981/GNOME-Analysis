1edf16d5dd4e1ebeb632cc61a8c1a9f6fb9584e7
Marcin Nastała <marcin.nastala98@op.pl>
2022-07-11 19:29:41 +0200

    GNOME magnetometers added; str and repr functions in magnetometer class

--- src/magnetometer.py
+++ src/magnetometer.py
@@ -1,53 +1,124 @@
 from typing import NamedTuple
+
+
 import scipy.constants as const
+
+
 import numpy as np
+
+
 import math
 
+
+
 #Earth radius in meters
+
+
 EARTH_RADIUS = 6.371e6
+
+
 #Domain wall speed in meters per second
+
+
 DOMAIN_WALL_SPEED = const.speed_of_light
 
+
+
 #1 degree in radians
+
+
 DEGREE_RAD = const.pi / 180.0
 
+
+
 #WGS-84 Axes
+
+
 SEMI_MAJOR_AXIS = 6.378137e6
+
+
 SEMI_MINOR_AXIS = 6.356752314245e6
+
+E
 
 
 class Coordinates(NamedTuple):
     lat: float
     lon: float
+
     h: float
     alt: float
+
     az: float
 
+
 class Position(NamedTuple):
+
     x: float
+
     y: float
+
     z: float
     
 
+
 class Signal(NamedTuple):
     time: float
+
     value: float
 
+
 class Magnetometer:
-    def __init__(self, noise: float, coordinates: Coordinates):
-        pass
-    def __LLH2EGEC(self, coordinates:Coordinates):
-        latRad = np.deg2rad(coordinates.lat)
-        lonRad = np.deg2rad(coordinates.lon)
-        e = math.sqrt(SEMI_MAJOR_AXIS * SEMI_MAJOR_AXIS - SEMI_MINOR_AXIS * SEMI_MINOR_AXIS) / SEMI_MAJOR_AXIS
-        cosphi = np.cos(lonRad)
-        sinphi = np.sin(lonRad)
-        coslambda = np.cos(latRad)
-        sinlambda = np.sin(latRad)
-        n = SEMI_MAJOR_AXIS / math.sqrt(1 - e * e * sinphi * sinphi)
+
+    def __init__(self, city: str, noise: float, coordinates: Coordinates):
+
+        self.__noise = noise * const.pico
+
+        self.__name = city
+
+        self.__LLH2ECEF(coordinates)
         
+
+    def __LLH2ECEF(self, coordinates: Coordinates):
+        
+        latRad = np.deg2rad(coordinates.lat)
+
+        lonRad = np.deg2rad(coordinates.lon)
+
+
+        cosphi = np.cos(lonRad)
+
+        sinphi = np.sin(lonRad)
+
+
+        coslambda = np.cos(latRad)
+
+
+        sinlambda = np.sin(latRad)
+
+
+        n = SEMI_MAJOR_AXIS / math.sqrt(1.0 - E * E * sinphi * sinphi)
+        
+
+
         self.__x = (n + coordinates.h) * cosphi * coslambda
+
+
         self.__y = (n + coordinates.h) * cosphi * sinlambda
-        self.__z = (n * (1 - e * e) + coordinates.h) * sinphi
+
+
+        self.__z = (n * (1.0 - E * E) + coordinates.h) * sinphi
+        
+    def __str__(self):
+
+
+        return f"{self.__name} -  Noise: {self.__noise}pT Location: [{self.__x}, {self.__y}, {self.__z}]m"
+    def __repr__(self):
+
+
+        return f"Name: {self.__name} - Noise: {self.__noise} - x: {self.__x} - y: {self.__y} - z: {self.__z}"
+    
         
     
+
+
--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
 import zipfile, os, sys
 import numpy as np
-from magnetometer import Signal, Magnetometer
+from magnetometer import Signal, Magnetometer, Coordinates
 
 
-sensors: dict[str, Magnetometer] = {}
+sensors: dict[str, Magnetometer] = {
+    "Krakow": Magnetometer("Krakow", 15.6, Coordinates(50.0289, 19.9048, 219.0, 45.0, 0.0)),
+    "Beijing": Magnetometer("Beijing", 10.4, Coordinates(40.2457, 116.1868, 44.0, 251.0, 0.0)),
+    "Berkeley": Magnetometer("Berkeley", 14.5, Coordinates(37.8723, -122.2570, 52.0, 0.0, 90.0)),
+    "Daejeon": Magnetometer("Daejeon", 116.0, Coordinates(36.3909, 127.3987, 72.0, 0.0, 90.0)),
+    "Fribourg": Magnetometer("Fribourg", 12.6, Coordinates(37.6564, 7.1581, 610.0, 190.0, 0.0)),
+    "Hayward": Magnetometer("Hayward", 14.3, Coordinates(46.7930, -122.0539, 34.0, 0.0, -90.0)),
+    "Hefei": Magnetometer("Hefei", 12.0, Coordinates(31.8429, 117.2526, 37.0, 90.0, 0.0)),
+    "Lewisburg": Magnetometer("Lewisburg", 54.5, Coordinates(40.9557, -76.8825, 634.0, 0.0, 90.0)),
+    "Mainz": Magnetometer("Mainz", 6.8, Coordinates(49.9915, 8.2354, 89.0, 0.0, -90.0))
+    
+}
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -12,16 +23,17 @@ if __name__ == "__main__":
     
     path = f"./data/{sys.argv[1]}.zip"
     
+    print(sensors)
+    
     magnetometers: list[list[Signal]] = []
     
     with zipfile.ZipFile(path) as zip:
         for name in zip.namelist():
             if zipfile.Path(path, name).is_file():
                 magnetometers.append([])
-                print(data)
-                # with zip.open(name) as file:
-                #     for line in file:
-                #         time, value = list(map(lambda x: float(x), line.decode().strip().split(" ")))
-                #         signal = Signal(time, value)
-                #         magnetometers[len(magnetometers) - 1].append(signal)
-    print(magnetometers)
+                with zip.open(name) as file:
+                    for line in file:
+                        time, value = list(map(lambda x: float(x), line.decode().strip().split(" ")))
+                        signal = Signal(time, value)
+                        magnetometers[len(magnetometers) - 1].append(signal)
+    # print(magnetometers)
